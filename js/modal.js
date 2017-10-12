1ed798ca2a6f357042ee6ba4804941e3f68067de
MixX0 <31200944+MixX0@users.noreply.github.com>
2017-10-13 01:10:28 +0300

    pixel perfect

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,18 @@
 var headerMainBtn = document.querySelector('.header-main__popup-button'),
   mainNav = document.querySelector('.main-nav'),
   logoMain = document.querySelector('.header-main__logo'),
-  headerMainIntro = document.querySelector('.header-main__intro'),
   popupError = document.querySelector('.popup-error'),
   popupDone = document.querySelector('.popup-done'),
   errorBtn = document.querySelector('.popup-error__button'),
-  doneBtn = document.querySelector('.popup-done__button');
+  doneBtn = document.querySelector('.popup-done__button'),
+  titlePadding = document.querySelector('.header-main__title--padding'),
+  introFoto = document.querySelector('.header-main__intro--photo'),
+  introForm = document.querySelector('.header-main__intro--form'),
+  introIndex = document.querySelector('.header-main__intro--index');
 
 mainNav.classList.remove('main-nav--opened');
 logoMain.classList.remove('header-main__logo--position');
-headerMainIntro.classList.remove('header-main__intro--padding');
+titlePadding.classList.remove('header-main__title--padding');
 headerMainBtn.classList.remove('header-main__popup-button--none');
 
 // MODAL MENU
@@ -20,13 +23,20 @@ function modalMenu() {
       headerMainBtn.classList.add('header-main__popup-button--opened');
       mainNav.classList.add('main-nav--opened');
       logoMain.classList.add('header-main__logo--position');
-      headerMainIntro.classList.add('header-main__intro--padding');
+      titlePadding.classList.add('header-main__title--padding');
+      introFoto.classList.remove('header-main__intro--photo');
+      introForm.classList.remove('header-main__intro--form');
+      introIndex.classList.remove('header-main__intro--index');
+
     } else {
       headerMainBtn.classList.remove('header-main__popup-button--opened');
       headerMainBtn.classList.add('header-main__popup-button--closed');
       mainNav.classList.remove('main-nav--opened');
       logoMain.classList.remove('header-main__logo--position');
-      headerMainIntro.classList.remove('header-main__intro--padding');
+      titlePadding.classList.remove('header-main__title--padding');
+      introFoto.classList.add('header-main__intro--photo');
+      introForm.classList.add('header-main__intro--form');
+      introIndex.classList.add('header-main__intro--index');
     }
   });
 }
