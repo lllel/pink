e76b41464aa0e170e6dec2416c83283936c79222
MixX0 <31200944+MixX0@users.noreply.github.com>
2017-10-15 23:14:10 +0300

    fix

--- js/picturefill.js
+++ js/picturefill.js
@@ -1,3 +1,4 @@
+/* stylelint-disable */
 /*! picturefill - v3.0.2 - 2016-02-12
  * https://scottjehl.github.io/picturefill/
  * Copyright (c) 2016 https://github.com/scottjehl/picturefill/blob/master/Authors.txt; Licensed MIT
