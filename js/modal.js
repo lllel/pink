a8e1862496b44525a23595d92c22f11feefe125c
MixX0 <31200944+MixX0@users.noreply.github.com>
2017-10-21 21:32:50 +0300

    Проблема с шапкой и js

--- js/modal.js
+++ js/modal.js
@@ -6,14 +6,15 @@ var headerMainBtn = document.querySelector('.header-main__popup-button'),
   errorBtn = document.querySelector('.popup-error__button'),
   doneBtn = document.querySelector('.popup-done__button'),
   titlePadding = document.querySelector('.header-main__title--padding'),
-  bgHeadPadding = document.querySelector('.header-main__bg-hand--padding'),
+  // bgHeadPadding = document.querySelector('.header-main__bg-hand--padding'),
   introBgPosition = document.querySelector('.header-main__intro--bg-position');
+  // bgHandPadding = document.querySelector('.header-main__bg-hand--padding');
 
 mainNav.classList.remove('main-nav--opened');
 logoMain.classList.remove('header-main__logo--position');
 titlePadding.classList.remove('header-main__title--padding');
 headerMainBtn.classList.remove('header-main__popup-button--none');
-bgHeadPadding.classList.remove('header-main__bg-hand--padding');
+// bgHeadPadding.classList.remove('header-main__bg-hand--padding');
 introBgPosition.classList.remove('header-main__intro--bg-position');
 
 // if (bgHeadPadding && introBgPosition) {
@@ -38,8 +39,9 @@ function modalMenu() {
       mainNav.classList.add('main-nav--opened');
       logoMain.classList.add('header-main__logo--position');
       titlePadding.classList.add('header-main__title--padding');
-      bgHeadPadding.classList.add('header-main__bg-hand--padding');
+      // bgHeadPadding.classList.add('header-main__bg-hand--padding');
       introBgPosition.classList.add('header-main__intro--bg-position');
+      // bgHandPadding.classList.add('.header-main__bg-hand--padding');
 
     } else {
       headerMainBtn.classList.remove('header-main__popup-button--opened');
@@ -47,8 +49,9 @@ function modalMenu() {
       mainNav.classList.remove('main-nav--opened');
       logoMain.classList.remove('header-main__logo--position');
       titlePadding.classList.remove('header-main__title--padding');
-      bgHeadPadding.classList.remove('header-main__bg-hand--padding');
+      // bgHeadPadding.classList.remove('header-main__bg-hand--padding');
       introBgPosition.classList.remove('header-main__intro--bg-position');
+      // bgHandPadding.classList.remove('.header-main__bg-hand--padding');
     }
   });
 }
